bbab69f2cc77b9784bebbbb7042e89eaf8714020
Corolaine <contacaroline.coutinho@gmail.com>
2024-07-22 07:59:06 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -29,6 +30,7 @@ public class Usuario {
     @NotNull(message = "O Atributo Nome é Obrigatório!")
     private String nome;
 
+    @Schema(example = "email@email.com.br")
     @NotNull(message = "O Atributo Usuário é Obrigatório!")
     @Email(message = "O Atributo Usuário deve ser um email válido!")
     @Column(nullable = false, unique = true)
@@ -44,6 +46,7 @@ public class Usuario {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
     @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
+
 
     public Usuario(Long id, String nome, String usuario, String senha, String foto) {
         this.id = id;
